3f236d6166b5d793cea77ce90be05021a71d0c63
Lakshya1115 <137154151+Lakshya1115@users.noreply.github.com>
2024-03-02 19:32:15 +0530

    login auth

--- login/loginPage.js
+++ login/loginPage.js
@@ -1,3 +1,38 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getDatabase, red, get, set, child } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAD6Uj40KwrKsdeEmUGsvo8lNwebuc0OYo",
+    authDomain: "guidegenie-a2177.firebaseapp.com",
+    projectId: "guidegenie-a2177",
+    storageBucket: "guidegenie-a2177.appspot.com",
+    messagingSenderId: "820065377823",
+    appId: "1:820065377823:web:a12d42fde4c173827a3f2a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+//   get ref to datat services
+  const db = getDatabase(ref);
+
+  document.getElementById("submit").addEventListener('click', function(e){
+
+    set(ref(db, 'name/' + document.getElementById("name").value),{
+        FullName: document.getElementById("name").value,
+        UserName: document.getElementById("user").value,
+        Email: document.getElementById("email").value,
+        Password: document.getElementById("pass").value,
+
+    });
+        alert("Login Successful");
+  })
